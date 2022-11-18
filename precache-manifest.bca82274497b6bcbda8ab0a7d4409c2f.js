51b3e1c201e0022292da7aa73793c653ef8a6ea3
Arjun E <arjunrk907@gmail.com>
2022-11-18 23:41:14 +0530

    Updates

---
+++ precache-manifest.bca82274497b6bcbda8ab0a7d4409c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d93be82b68f236988de",
+    "url": "/TodoApp/static/css/main.e512ed7d.chunk.css"
+  },
+  {
+    "revision": "9d93be82b68f236988de",
+    "url": "/TodoApp/static/js/main.9d93be82.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/TodoApp/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "1b4a16ba3f2171f1c785",
+    "url": "/TodoApp/static/js/2.1b4a16ba.chunk.js"
+  },
+  {
+    "revision": "cb03f85a8633808c2bc2",
+    "url": "/TodoApp/static/js/runtime~main.cb03f85a.js"
+  },
+  {
+    "revision": "fe8c5cfef5da2cf8816a2b6378368e50",
+    "url": "/TodoApp/index.html"
+  }
+];
